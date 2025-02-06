7c1f2af307ab1042fae24003c1fb73b7aa1913a7
CesarUMx <cortiz@mondragonmexico.edu.mx>
2025-02-06 11:37:28 -0600

    dejar al 100 main

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,17 +4,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import auth from 'auth-astro';
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), auth()],
+  integrations: [react(), tailwind()],
   server: {
     port: 3001,
   },
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-const { signIn, signOut } = await import("auth-astro/client")
 import { getBackendURL } from "../utils/url";
+
 const LoginComponent = () => {
+
+    const googleLoginURL = getBackendURL("/auth/google");
 
     // Estado para almacenar el mensaje de error
     const [errorMessage, setErrorMessage] = useState("");
@@ -13,18 +15,6 @@ const LoginComponent = () => {
             setErrorMessage(decodeURIComponent(error));
         }
     }, []);
-
-    // Evento para iniciar sesión con Google
-    const handleLogin = async () => {
-        try {
-            const response = await signIn("google", {
-                redirectTo: "/dashboard",
-            });
-            console.log(response);
-        } catch (error) {
-            console.error(error);
-        }
-    };
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
@@ -40,12 +30,12 @@ const LoginComponent = () => {
                 )}
 
                 <div className="text-center">
-                    <button className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded flex items-center justify-center gap-2" onClick={handleLogin}>
+                    <a href={googleLoginURL} className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded flex items-center justify-center gap-2">
                          <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fillRule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clipRule="evenodd"/>
                          </svg>
                         Iniciar sesión con Google
-                    </button>
+                    </a>
                 </div>
             </div>
         </div>
